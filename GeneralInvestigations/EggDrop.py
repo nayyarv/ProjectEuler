ab42594062b6b8b94958f264c74772e070ca4990
Varun Nayyar <nayyarv@gmail.com>
2013-04-22 18:57:51 +1000

    Updated Readme and some memoisation work

--- GeneralInvestigations/EggDrop.py
+++ GeneralInvestigations/EggDrop.py
@@ -1,8 +1,19 @@
 __author__ = 'Varun Nayyar'
 __date__ = "22/04/13 6:29 PM"
 
+from memoisationPython import memoized
+
 
 def main():
+    pass
+
+
+@memoized
+def EggDrop(numEggs, numFloors):
+    #TODO: Stuff
+
+    #Step up, and assume egg breaks
+
     pass
 
 
